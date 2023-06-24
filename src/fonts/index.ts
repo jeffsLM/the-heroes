cac3e416cfa8fe35a5a2f734923f20a20be767f2
jeffsLM <jeffes.dev@gmail.com>
2023-06-24 20:10:59 -0300

    [add]: font pre loader

---
+++ src/fonts/index.ts
@@ -0,0 +1,6 @@
+import { Arimo } from 'next/font/google'
+
+export const arimo = Arimo({
+  subsets: ['latin'],
+  variable: '--arimo-font-family'
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeContextProvider } from '../contexts/Theme.context';
+
+import { arimo } from '../fonts';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeContextProvider>
+      <main className={arimo.className}>
+        <Head>
+          <title>my-hero-project</title>
+        </Head>
+        <Component {...pageProps} />
+      </main>
+    </ThemeContextProvider>
+  )
 }
