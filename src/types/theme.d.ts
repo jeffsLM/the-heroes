10f734f0dfa1b54a632d7dedb73b3a0ff2044cde
jeffsLM <jeffes.dev@gmail.com>
2023-06-27 16:33:07 -0300

    [fix]: ts error auto complete

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -1,1 +1,22 @@
 export type ThemeProps = 'light' | 'dark';
+
+export type ThemeContextType = {
+  colors: {
+    primary: string;
+    secondary: string;
+    tertiary: string;
+  };
+  shadesOfGray: {
+    i1000: string;
+    i900: string;
+    i800: string;
+    i700: string;
+  };
+  background: string;
+  fonts: {
+    light: number;
+    regular: number;
+    medium: number;
+    bold: number;
+  };
+};
